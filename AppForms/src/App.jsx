1963f9f030186867490383acc737a836651ca618
BrianD10_iD <155474051+BrianD10iD@users.noreply.github.com>
2024-08-23 02:24:11 +0000

    d

--- AppForms/src/App.jsx
+++ AppForms/src/App.jsx
@@ -1,10 +1,16 @@
 import { Fragment } from "react";
-import Formulario from "./componte/Formulario";
+import Formulario from './componte/Formulario'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
-function app(){
-  return(
+function app() {
+  return (
     <>
-      <Formulario/>
+      <div className="container container-fluid">
+        <br />
+        <br />
+        <h1>Ingreso de Datos</h1>
+        <Formulario/>
+      </div>
     </>
   );
 }
